8c7af4812a842348480ee684ee32659d00c1c525
Bradley Lund <bradjlund@gmail.com>
2020-10-04 20:44:20 +0200

    fetch data

--- heat.js
+++ heat.js
@@ -1,1 +1,4 @@
-fetch() //put in the address for fetching the data and then put it in here
+
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json') //put in the address for fetching the data and then put it in here
+.then(response => response.json())
+.then(data => console.log(data));
