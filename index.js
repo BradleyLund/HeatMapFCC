eb1873b009655bf461d2448da8a6e4dfba07d2c4
Bradley Lund <bradjlund@gmail.com>
2020-10-20 22:20:58 +0200

    start x axis

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const svg = d3.select('svg');
-svg.style('background-color','red');
-
-const circle = svg.append('circle');
 
 const height = +svg.attr('height');
 const width = +svg.attr('width');
 
-console.log(typeof width)
+const padding = 50;
 
-circle.attr('r',height / 2);
+var xAxisScale = d3.scaleLinear() //could be issues with the type of scale
+    .range([padding,width-padding]);
 
-circle.attr('cx',width/2);
+var yAxisScale = d3.scaleLinear()
+    .range([0,height-padding]);
 
-circle.attr('cy', height/2);
+var xAxis = d3.axisBottom(xAxisScale)
+    .tickFormat(d3.format("d"))
+
+var gX = svg.append("g")
+    .attr('id','x-axis')
+    .attr('transform','translate(0,'+(height-padding)+')')
+    .call(xAxis)
+
+
+
