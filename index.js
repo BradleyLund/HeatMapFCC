3da61da09b0a7322f4f65fef2a1aa9c11c45fb52
Bradley Lund <bradjlund@gmail.com>
2020-10-29 19:00:05 +0100

    try add scales

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
   .then(response => response.json())
   .then(data=> {
 
-    console.log(data)
 
     const svg = d3.select('svg');
 
@@ -14,22 +13,42 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     var xAxisScale = d3.scaleLinear() //could be issues with the type of scale
         .range([padding,width-padding]);
 
-    var yAxisScale = d3.scaleLinear()
-        .range([0,height-padding]);
+    const yScale = d3.scaleTime()
+        .domain([new Date().setMonth(0),new Date().setMonth(12)])
+        .range([padding, height-padding]);
+
+    const yAxis = d3.axisLeft(yScale)
+        .ticks(12)
+        .tickFormat(d3.timeFormat("%B"));
+
 
     var xAxis = d3.axisBottom(xAxisScale)
         .tickFormat(d3.format("d"))
 
-   
+    
 
-    xAxisScale.domain(d3.extent(data,d=>{
-        return d.Year
-    }))
+
+    let yearArr = []
+    for (let i =0;i<data.monthlyVariance.length;i+=12) {
+        yearArr.push(data.monthlyVariance[i].year)
+    }
+
+    console.log(d3.extent(yearArr))
+
+
+
+    xAxisScale.domain(d3.extent(yearArr));
+
 
     var gX = svg.append("g")
     .attr('id','x-axis')
     .attr('transform','translate(0,'+(height-padding)+')')
     .call(xAxis)
+
+    var gY = svg.append("g")
+    .attr("transform","translate("+padding+",0)")
+    .attr('id','y-axis')
+    .call(yAxis)
 
 
   })
