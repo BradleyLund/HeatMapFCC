ebb1b74ac7f20f37b664262e0528b81b268016e9
Bradley Lund <bradjlund@gmail.com>
2020-11-07 14:03:17 +0200

    just tooltip left

--- index.js
+++ index.js
@@ -81,19 +81,57 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     // we need to get the x coordinate from the year and the y coordinates from the month,
     //lets just try with the first one
 
-    let janRect = [data.monthlyVariance[0]]
-    console.log(xAxisScale(janRect.year),z(new Date(2000,janRect.month-1,01)),rectHeight)
+    
+   
+
+    function colorPicker(temp) {
+        if (temp < 3.9) {return "#4575B4"}
+        else if(temp<5) {return "#74ADD1"}
+        else if(temp<6.1) {return "#ABD9E9"}
+        else if(temp<7.2) {return "#E0F3F8"}
+        else if(temp<8.3) return "#FFFFBF"
+        else if(temp<9.5) return "#FEE090"
+        else if(temp<10.6) return "#FDAE61"
+        else if(temp<11.7) return "#F46D43"
+        else {return "#D73027"}
+
+    }
+
+    console.log(colorPicker(3))
 
     svg.append('g')
-      .selectAll("dot")
+      .selectAll("rect")
       .data(data.monthlyVariance)
       .enter()
       .append("rect")
+      .attr('class','cell')
+      .attr('data-month',d =>d.month-1)
+      .attr('data-year',d=>d.year)
+      .attr('data-temp',d=>8.66+d.variance)
       .attr('x',d=>{return xAxisScale(d.year)})
       .attr('y',d=>{return z(new Date(2000,d.month-1,01))})
+      .attr('fill',d=>{return colorPicker(8.66+d.variance)})
       .attr('height',rectHeight)
       .attr('width',rectWidth)
-  
+      
+
+    let dataLegend = [{temp:3},{temp:4.5},{temp:5.5},{temp:6.6},{temp:7.7},{temp:8.8},{temp:9.9},{temp:11.1},{temp:12}]
+
+
+
+    svg.append('g')
+      .attr('id','legend')
+      .selectAll('rect')
+      .data(dataLegend)
+      .enter()
+      .append("rect")
+      .attr('x',(d,i)=>100+(i*30))
+      .attr('y',height-50)
+      .attr('fill',d=>{return colorPicker(d.temp)})
+      .attr('height',30)
+      .attr('width',30)
+      .attr('stroke','black')
+
 
 
 
